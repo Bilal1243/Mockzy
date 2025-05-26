541cc879da9979327f5027597406025bd4984c1a
Muhammed Bilal <130375693+Bilal1243@users.noreply.github.com>
2025-05-26 14:16:49 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,6 +4,8 @@ import { authUser, logoutUser } from '../controllers/userControllers.js'
 
 const userRoute = express()
 
+console.log('entered)
+
 userRoute.route('/').post(authUser)
 
 userRoute.route('/logout').get(logoutUser)
