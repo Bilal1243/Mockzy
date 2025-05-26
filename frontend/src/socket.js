71a8d6b1e22cbf16ac11f66e11604448d1b6c4cb
Muhammed Bilal <130375693+Bilal1243@users.noreply.github.com>
2025-05-26 12:15:04 +0530

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://mockzy-backend.onrender.com", {
   transports: ["websocket"], // force websocket
   reconnectionAttempts: 5,   // optional: retry 5 times
 });
